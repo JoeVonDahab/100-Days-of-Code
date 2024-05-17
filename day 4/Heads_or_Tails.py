f70f7323fbae7e2d2ebe7791c2ce6dd5bdfccbdc
Youssef Abo-Dahab <168999720+JoeVonDahab@users.noreply.github.com>
2024-05-17 15:02:42 +0300

    Create Heads_or_Tails.py

---
+++ day 4/Heads_or_Tails.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+headsortails = random.randint(0,1)
+if headsortails == 0:
+  print ('Tails')
+else:
+  print ("Heads")
+input('press any key to continue')
