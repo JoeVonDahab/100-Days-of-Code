e7be2b6407afaa46f47b1e5b097aa43cb06df7c3
Youssef Abo-Dahab <168999720+JoeVonDahab@users.noreply.github.com>
2024-05-19 01:36:20 +0300

    Create position_in_map.py

---
+++ day 4/position_in_map.py
@@ -0,0 +1,17 @@
+# Initial map setup
+row1 = ['⬜️', '⬜️', '⬜️']
+row2 = ['⬜️', '⬜️', '⬜️']
+row3 = ['⬜️', '⬜️', '⬜️']
+map = [row1, row2, row3]
+# Display the initial map
+user_input = input('Enter a postion: Examle "B2 or A1"')
+letter = user_input[0].lower()
+abc = ['a','b','c']
+letter_index = abc.index(letter)
+number_index = int(user_input[1]) - 1
+map[number_index][letter_index] = 'X'
+
+# Display the updated map
+print('Hiding your treasure! X marks the spot.')
+print(f"{row1}\n{row2}\n{row3}")
+input('press')
